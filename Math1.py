c08b0c89b2152b39193b4b446e570a49ff9b0221
Sviatoslav <s.mudryy_fit_9_26_b_d@knute.edu.ua>
2026-09-18 17:15:14 +0300

    upload

---
+++ Math1.py
@@ -0,0 +1,15 @@
+import math
+
+x = float(input("Enter a number x: "))
+
+numeration = math.cos(x) + math.log(abs(x) + 1)
+denominator = math.exp(0.7 * x) + math.pow(4, 3.3 * x + 1)
+
+if x >= 0:
+    second_term = 0.9 * math.pow(x, math.sqrt(2 * x))
+else:
+    raise ValueError("x must be non-negative for the second term calculation.")
+
+f_x = (numeration / denominator) - second_term
+
+print("The value of the function f(x) is:", f_x)
