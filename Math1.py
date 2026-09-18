16b26c1de2f41831cc84a1626b637d9f9a6a35ef
Sviatoslav <s.mudryy_fit_9_26_b_d@knute.edu.ua>
2026-09-18 21:08:40 +0300

    Upd

--- Math1.py
+++ Math1.py
@@ -7,9 +7,8 @@ denominator = math.exp(0.7 * x) + math.pow(4, 3.3 * x + 1)
 
 if x >= 0:
     second_term = 0.9 * math.pow(x, math.sqrt(2 * x))
+    f_x = (numeration / denominator) - second_term
+    print("The value of the function f(x) is:", f_x)
 else:
     raise ValueError("x must be non-negative for the second term calculation.")
 
-f_x = (numeration / denominator) - second_term
-
-print("The value of the function f(x) is:", f_x)
